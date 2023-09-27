e155308f0e8f53856069de1391dd11f5bb0fa320
Josohp <jadistan4@gmail.com>
2023-09-27 08:39:45 -0700

    DONE

---
+++ Joseph.py
@@ -0,0 +1,18 @@
+    # num1 = int(input('Enter the number 1 :'))
+    # num2 = int(input('Enter the number 2: '))
+    # num2 = int(input('Enter the number 3: '))
+    num1 = 600
+    num2 = 300
+    num3 = 400
+
+    if num1 < num2:
+        if num1 < num3:
+            minval = num1
+        else:
+            minval = num3
+    else:
+        if num2 < num3:
+            minval = num2
+        else:
+            minval = num3
+    print (minval)   
